1024ccf21ccb51ea2f2b73907044db4b7e38df79
Anil Kag <anilkagak2@gmail.com>
2013-04-12 20:29:51 +0530

    - Forgot to add the two files in last commit 	- FetchPosts --> fetches various posts available in the system 		& keeps in session for future accesses 	- PostsFilter --> puts posts object in each access to login.jsp 		resource so that the posts drop-down is shown on login.jsp

---
+++ src/server/FetchPosts.java
@@ -0,0 +1,90 @@
+package server;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class FetchPosts
+ */
+@WebServlet("/FetchPosts")
+public class FetchPosts extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private Connection connection;
+    private int loginId=0;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public FetchPosts() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	protected void closeConnection () {
+		if (connection != null) {
+            try {
+                connection.close();
+                System.out.println("Database connection terminated");
+            } catch (Exception e) {
+            	System.out.println(e.toString()+ "\n Exception Stack: \n");
+                e.printStackTrace();
+            }
+        }
+	}
+	
+	protected void processRequest (HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println ("In fetchposts");
+		try{
+			String mysqlUser = Declarations.mysqlUser;
+			String mysqlPass = Declarations.mysqlPass;
+			String url = Declarations.url;
+			Class.forName("com.mysql.jdbc.Driver").newInstance();
+			connection = DriverManager.getConnection(url, mysqlUser, mysqlPass);
+
+			if(connection!=null){
+				List<String> posts = Declarations.fetchPosts(connection);
+				request.setAttribute("posts",posts);
+			}
+			else{
+				System.out.println("Database connection failed.");
+				request.getRequestDispatcher(Declarations.loginHome).forward(request, response);
+				return;
+			}
+
+			closeConnection();
+			request.getRequestDispatcher(Declarations.loginHome).forward(request, response);
+		}catch(Exception e)
+		{
+			closeConnection();
+			System.out.println("Fetch Details"+e.toString()+ "\n Exception Stack: \n");
+			e.printStackTrace();
+			request.getRequestDispatcher(Declarations.loginHome).forward(request, response);
+		}
+	}
+	
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		processRequest(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		processRequest(request, response);
+	}
+}
---
+++ src/server/PostsFilter.java
@@ -0,0 +1,50 @@
+package server;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+
+/**
+ * Servlet Filter implementation class PostsFilter
+ */
+@WebFilter("/PostsFilter")
+public class PostsFilter implements Filter {
+
+    /**
+     * Default constructor. 
+     */
+    public PostsFilter() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Filter#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+	 */
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		System.out.println("In FilterPosts");
+		if (request.getAttribute("posts") == null) 
+			request.getRequestDispatcher("/FetchPosts").forward(request, response);
+		else
+			request.getRequestDispatcher("/Login.jsp").forward(request, response);
+	}
+
+	/**
+	 * @see Filter#init(FilterConfig)
+	 */
+	public void init(FilterConfig fConfig) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+}
